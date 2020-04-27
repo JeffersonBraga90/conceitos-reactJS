0574798ae7e8305e72d9a091c1f279d385c39e50
Jefferson Corrêa Braga <jefferson.braga.correa@gmail.com>
2020-04-27 15:45:20 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { useEffect, useState } from "react";
-import api from './services/api';
+import api from "./services/api";
 
 import "./styles.css";
 
-const [repositories, setRepositories] = useState([]);
-
-useEffect(() => {
-  api.get('repositories').then(response => {
-    setRepositories(response.data);
-  });
-}, []);
-
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    const response = await api.post('repositories', {
-      title: 'Umbriel',
-      url: 'https://github.com/rocketseat/umbriel',
-      techs: ['Node.js', 'ReactJS']
-     });
-    
-     setRepositories([...repositories, response.data]); 
+    const response = await api.post("repositories", {
+      title: "Umbriel",
+      url: "https://github.com/rocketseat/umbriel",
+      techs: ["Node.js", "ReactJS"],
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
     await api.delete(`repositories/${id}`);
 
     const newRepositories = repositories.filter(
-      repository => repository.id !== id
-    )
+      (repository) => repository.id !== id
+    );
 
     setRepositories(newRepositories);
   }
@@ -35,14 +35,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        {repositories.map(repository => (
+        {repositories.map((repository) => (
           <li key={repository.id}>
             {repository.title}
 
             <button onClick={() => handleRemoveRepository(repository.id)}>
-            Remover
+              Remover
             </button>
-          </li>  
+          </li>
         ))}
       </ul>
 
